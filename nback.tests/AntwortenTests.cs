ea2e54cbf575d616c4de9993ca247a6e7f7a01f9
Thomas Leidl <toom-l@hotmail.de>
2017-06-10 14:42:11 +0200

    Antworten Tests

---
+++ nback.tests/AntwortenTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nback.data.data;
+using nback.logik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nback.tests
+{
+    [TestClass]
+    public class AntwortenTests
+    {
+        [TestMethod]
+        public void Fünf_Antworten_werden_gegeben()
+        {
+            var sut = new Antworten();
+            sut.Antwort_registrieren(Antwort.Keine_Wiederholung);
+            sut.Antwort_registrieren(Antwort.Keine_Wiederholung);
+            sut.Antwort_registrieren(Antwort.Wiederholung);
+            sut.Antwort_registrieren(Antwort.Keine_Wiederholung);
+            sut.Antwort_registrieren(Antwort.Keine_Wiederholung);
+
+            Assert.AreEqual(5, sut.Antwortenliste.Count());
+            Assert.AreEqual(Antwort.Keine_Wiederholung, sut.Antwortenliste.ElementAt(0));
+            Assert.AreEqual(Antwort.Wiederholung, sut.Antwortenliste.ElementAt(2));
+        }
+    }
+}
