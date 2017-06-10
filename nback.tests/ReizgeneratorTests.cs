7f25e6e9a4bc1938ef868c838d5a0e24e8799bee
Thomas Leidl <toom-l@hotmail.de>
2017-06-10 15:08:18 +0200

    Reizgenerator Tests

---
+++ nback.tests/ReizgeneratorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nback.data.data;
+using nback.logik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nback.tests
+{
+    [TestClass]
+    public class ReizgeneratorTests
+    {
+        [TestMethod]
+        public void Reizfolge_berechnen_5_Reize_eine_Wiederholung()
+        {
+            var reizgenerator = new Reizgenerator(new ZufallsgeneratorMock());
+            var sut = reizgenerator.Reizfolge_berechnen(5, 2);
+
+            var reize = new Reiz[] { new Reiz('A', 5, 1), new Reiz('B', 5, 2),
+                                     new Reiz('A', 5, 3), new Reiz('D', 5, 4), new Reiz('E', 5, 5) };
+            var expected = new Queue<Reiz>(reize);
+
+            Assert.IsTrue(expected.SequenceEqual(sut.ToArray(), new ReizEqualityComparer()));
+        }
+    }
+}
